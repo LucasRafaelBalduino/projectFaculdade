49afd759ebe9cec4f563b5a9985bba8dfad190b1
Lucas Balduino <lukasrafael1220@gmail.com>
2020-05-11 01:34:03 -0300

    :sparkles: add Endpoint client

---
+++ src/main/java/com/example/cursomc/services/ClienteService.java
@@ -0,0 +1,24 @@
+package com.example.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.cursomc.domain.Cliente;
+import com.example.cursomc.repositories.ClienteRepository;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+
+	public Cliente find(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+
+		return obj.orElseThrow(() -> new com.example.cursomc.services.exceptions.ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+
+}
